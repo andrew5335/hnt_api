e9e37282d5e8ca113d22d2d480b5b52331f7e80a
andrew <andrew2767@gmail.com>
2022-07-20 00:26:03 +0900

    initial commit

---
+++ src/main/java/com/andrew/hnt/api/HntSensorApiApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.hnt.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HntSensorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HntSensorApiApplication.class, args);
+	}
+
+}
