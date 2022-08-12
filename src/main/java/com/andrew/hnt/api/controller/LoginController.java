b293255fac21a15b13d8d7f78bc7062e5a023f64
Andrew <kim.hyoungju@kt.com>
2022-08-12 19:28:31 +0900

    로그인 및 회원가입 관련 컨트롤러 추가

--- src/main/java/com/andrew/hnt/api/controller/LoginController.java
+++ src/main/java/com/andrew/hnt/api/controller/LoginController.java
@@ -1,8 +1,60 @@
 package com.andrew.hnt.api.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController extends DefaultController {
 
+	/**
+	 * 로그인 페이지 호출
+	 * @param req
+	 * @param res
+	 * @return
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(HttpServletRequest req, HttpServletResponse res) {
+		return "login";
+	}
+	
+	/**
+	 * 로그인 처리 요청
+	 * @param req
+	 * @param res
+	 * @return
+	 */
+	@RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
+	public String loginProcess(HttpServletRequest req, HttpServletResponse res) {
+		String result = "";
+		
+		return result;
+	}
+	
+	/**
+	 * 회원 가입 페이지 호출
+	 * @param req
+	 * @param res
+	 * @return
+	 */
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String join(HttpServletRequest req, HttpServletResponse res) {
+		return "join";
+	}
+	
+	/**
+	 * 회원 가입 처리 요청
+	 * @param req
+	 * @param res
+	 * @return
+	 */
+	@RequestMapping(value = "/joinProcess", method = RequestMethod.POST)
+	public String joinProcess(HttpServletRequest req, HttpServletResponse res) {
+		String result = "";
+		
+		return result;
+	}
 }
