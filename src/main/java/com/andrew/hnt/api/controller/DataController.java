b788aeca5f76feed8a66b760d52a9a74813c4ff5
Andrew <kim.hyoungju@kt.com>
2022-08-30 16:13:00 +0900

    페이지 추가

---
+++ src/main/java/com/andrew/hnt/api/controller/DataController.java
@@ -0,0 +1,10 @@
+package com.andrew.hnt.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/data")
+public class DataController extends DefaultController {
+
+}
